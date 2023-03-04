35a6ce4b7b9067c8c1b606ee84975e48444b2caf
Steve Ruben <stevetuenkam@gmail.com>
2023-03-04 10:04:34 +0000

    first commit

---
+++ src/test/java/org/abodah/sample/SpringSecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abodah.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
