e06eaa154c905b49d5be3dcd8d9f4cf10f0a8ac6
Steve Ruben <stevetuenkam@gmail.com>
2023-03-05 05:22:21 +0000

    writing first test

--- src/main/java/org/abodah/sample/controller/WebController.java
+++ src/main/java/org/abodah/sample/controller/WebController.java
@@ -1,6 +1,9 @@
 package org.abodah.sample.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,16 +17,32 @@ public class WebController {
         return "Welcome home!";
     }
 
-    @RequestMapping("/restricted")
+    @RequestMapping("/test")
     @ResponseBody
     public String restricted() {
         return "You found the secret lair!";
     }
 
-    @PreAuthorize("hasAuthority('Admin')")
+    @RequestMapping("/me")
+    @ResponseBody
+    @PreAuthorize("authenticated")
+    public String getMessage() {
+        Authentication authentication = SecurityContextHolder.getContext()
+                .getAuthentication();
+        return "Hello " + authentication;
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     @RequestMapping("/restricted")
     @ResponseBody
-    public String adminOnly() {
+    public String adminAuthoritiesOnly() {
+        return "You found the secret lair!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping("/restricted_role")
+    @ResponseBody
+    public String adminRole() {
         return "You found the secret lair!";
     }
 }
---
+++ src/test/java/org/abodah/sample/controller/WebControllerTest.java
@@ -0,0 +1,58 @@
+package org.abodah.sample.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(WebController.class)
+public class WebControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithAnonymousUser
+    @Test
+    public void givenNoAuth_shouldSucceedWith200() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(value = "spring",roles={"USER","ADMIN"})
+    @Test
+    public void givenAuthRequestOnPrivateService_shouldSucceedWith200() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/restricted_role").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(value = "spring",authorities={"USER","ADMIN"})
+    @Test
+    public void givenAuthRequest_shouldSucceedWith200() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/restricted").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
